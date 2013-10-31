f05b3e20d9541a0de27f51e1e7b3bd6a3f1e44ca
Nataliya <nataliya.zhukava@gmail.com>
2013-10-31 17:15:35 +0300

    new pages

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+$.fn.extend({
+  exists: function(callback) {
+    if (this.length > 0) {
+      if (callback != null) {
+        callback.call(this);
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+});
+
+jQuery(document).ready(function(){
+
+	//for slider
+	
+	$('[data-slider]').exists(function(){
+		$(this).flexslider({
+	        animation: "slide",
+	        controlNav: false
+	      });
+
+	});
+	
+	//for animate on index page
+	
+	$('[data-animate]').exists(function(){
+		$(this).on('click', function(e){
+			$(this).toggleClass("hover");
+			
+			e.preventDefault();
+		});
+	});
+	
+	// for e-mail form
+	
+	$('[data-form]').exists(function(){ 
+		$('[data-form="link"]').on('click', function(e){
+			
+			$('[data-form="form"]').toggleClass("show");
+			
+			e.preventDefault();
+			
+		});
+		
+		$(document).on('click', function(e){
+			if (!$(e.target).closest('[data-form="link"]').length) {
+				$('[data-form="form"]').removeClass('show');
+			}
+		});
+		
+	});
+	
+	// for recipe
+	
+	$('[data-recipe]').exists(function(){ //change for css
+		/*
+		$('[data-recipe="true"]').on('click', function(e){
+			$('[data-recipe="open"]').addClass("open");
+			e.preventDefault();
+		});
+		$('[data-cls]').on('click', function(e){
+			$('[data-recipe="open"]').removeClass("open");
+			e.preventDefault();
+		});
+		*/
+		$('[data-recipe="true"]').on('click', function(e){
+			$('[data-recipe="open"]').slideDown();
+			e.preventDefault();
+		});
+		$('[data-cls]').on('click', function(e){
+			$('[data-recipe="open"]').slideUp();
+			e.preventDefault();
+		});
+	});
+	
+});
