2807e8219f601ad05d964b7e9b670bbda2dd7ee2
Pavel Astraukh <paladin111333@yandex.ru>
2013-11-03 22:23:28 +0300

    Add video gallery thumb click event

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ $.fn.extend({
 jQuery(document).ready(function(){
 
 	//for slider
-	
+
 	$('[data-slider]').exists(function(){
 		$(this).flexslider({
 	        animation: "slide",
@@ -22,38 +22,38 @@ jQuery(document).ready(function(){
 	      });
 
 	});
-	
+
 	//for animate on index page
-	
+
 	$('[data-animate]').exists(function(){
 		$(this).on('click', function(e){
 			$(this).toggleClass("hover");
-			
+
 			e.preventDefault();
 		});
 	});
-	
+
 	// for e-mail form
-	
-	$('[data-form]').exists(function(){ 
+
+	$('[data-form]').exists(function(){
 		$('[data-form="link"]').on('click', function(e){
-			
+
 			$('[data-form="form"]').toggleClass("show");
-			
+
 			e.preventDefault();
-			
+
 		});
-		
+
 		$(document).on('click', function(e){
 			if (!$(e.target).closest('[data-form="link"]').length) {
 				$('[data-form="form"]').removeClass('show');
 			}
 		});
-		
+
 	});
-	
+
 	// for recipe
-	
+
 	$('[data-recipe]').exists(function(){ //change for css
 		/*
 		$('[data-recipe="true"]').on('click', function(e){
@@ -74,13 +74,22 @@ jQuery(document).ready(function(){
 			e.preventDefault();
 		});
 	});
-	
+
 	// for brand
-	$('[data-brand]').exists(function(){ //change for css		
+	$('[data-brand]').exists(function(){ //change for css
 		$('[data-brand="true"]').on('click', function(e){
 			$('[data-brand="open"]').slideToggle();
 			e.preventDefault();
 		});
 	});
-	
+
+	$('[data-gallery-item]').on('click', function(e){
+		e.preventDefault()
+		var galleryMain = $('[data-gallery-main]')
+		var mainSrc = galleryMain.attr('src')
+		galleryMain.attr('src', $(this).find('img').attr('src'))
+		$(this).find('img').attr('src', mainSrc)
+
+	})
+
 });
